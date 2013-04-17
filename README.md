bcb4be53521b3e1e49575e7a146306f7cbe43cf6
Edward Lee <eddieberklee@gmail.com>
2013-04-16 21:45:20 -0700

    progress

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ INFO 290T Project for Stocks Analysis/Visualization.
 
 URL: https://docs.google.com/document/d/1nZ2r7LgN9JV6OAuD50ngHUz0TTiRV6SZUkL9WlC6ECM/edit?usp=sharing
 
+TODO
+* Use PANDAS!
+
+
+
+
--- stock-slope.py
+++ stock-slope.py
@@ -1,13 +1,18 @@
+import urllib
+import re
+
+class Date:
+    # TODO: parse month-day-year
+    pass
 
 class Company:
     def __init__(self, name):
         self.name = name
         self.companyToSymbol() # sets self.symbol
+        self.releaseDates() # sets self.releaseDates
     def __str__(self):
         return str(self.__dict__.keys())
     def companyToSymbol(self):
-        import urllib
-        import re
         url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query=%s&callback=YAHOO.Finance.SymbolSuggest.ssCallback" % self.name
         u = urllib.urlopen(url)
         data = u.read()
@@ -16,7 +21,30 @@ class Company:
         symbol = symbol.split(':')[1][1:-1]
         self.symbol = symbol
         return symbol
+    def releaseDates(self):
+        self.release_dates = {
+            'iBooks Author' : '1-19-2012',
+            'iPad (3rd gen)' : '3-16-2012',
+            'Apple TV (3rd gen)' : '3-16-2012',
+            'Mac Pro (Mid 2012)' : '6-11-2012',
+            'Macbook Air (Mid 2012)' : '6-11-2012',
+        }
 appleCompany = Company("Apple")
+
+# TODO: enclose this into a method
+productDate = appleCompany.release_dates['iBooks Author']
+stockSymbol = appleCompany.symbol
+smonth = 1
+sday = 12
+syear = 2012
+emonth = 1
+eday = 26
+eyear = 2012
+url = "http://ichart.yahoo.com/table.csv?s=%s&a=%i&b=%i&c=%i&d=%i&e=%i&f=%i&g=d&ignore=.csv" % ( stockSymbol, smonth, sday, syear, emonth, eday, eyear)
+u = urllib.urlopen(url)
+for perDay in u.readlines():
+    print perDay.strip()
+
 print appleCompany
 
 
