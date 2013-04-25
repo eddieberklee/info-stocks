2152b56836d9f6089509a347f3d63b46549e7fea
youj3an <youj3an@gmail.com>
2013-04-25 03:28:29 -0700

    changed stockpercentage to discontinued date

--- stock-slope.py
+++ stock-slope.py
@@ -240,20 +240,11 @@ apple.getData() # defaults to time padding of 7 days
 datahash = getProductReleasesForApple()
 print datahash
 
-"""
 import pandas
 import matplotlib as plt
 
-timeline = {}
+timeline = datahash
 
-timeline['iPod'] = ['iPod Family', Date('12-12-12'), 25]
-timeline['iPod Touch'] = ['iPod Family', Date('1-1-1'), 11]
-timeline['iPhone'] = ['iPhone', Date('2-12-12'), 100]
-timeline['iMac'] = ['iMacs', Date('1-1-91'), -25]
-
-for item in timeline.items():
-    print item[1][1].dateSort()
-#sortedTimeline = timeline.items().sort(key=lambda tup: tup[1][1].dateSort())
 sortedTimeline = sorted(timeline.items(),key=lambda tup: tup[1][1].dateSort())
 
 print sortedTimeline
@@ -261,17 +252,19 @@ print sortedTimeline
 productName = []
 family = []
 releaseDate = []
-stockSlope = []
+discontinueDate = []
 
 for item in sortedTimeline:
     print item
     productName.append(item[0])
     family.append(item[1][0])
     releaseDate.append(item[1][1])
-    stockSlope.append(item[1][2])
+    discontinueDate.append(item[1][2])
 
-timelineDataFrame = pandas.DataFrame({'Product Name': productName, 'Family': family, 'Release Date':releaseDate, 'Stock Impact': stockSlope}).set_index('Product Name')
+timelineDataFrame = pandas.DataFrame({'Product Name': productName, 'Family': family, 'Release Date':releaseDate, 'Date Discontinued': discontinueDate}).set_index('Product Name')
 
-timelineDataFrame.plot(use_index=True, y='Stock Impact')
 
-"""
+
+#timelineDataFrame.plot(use_index=True, y='')
+
+
