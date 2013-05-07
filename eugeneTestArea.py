66fcf512dec313627dab7f0833365a21511988e8
youj3an <youj3an@gmail.com>
2013-05-07 02:00:11 -0700

    implemented interactive graph for Query.plotIndividualStockDifferences()

--- eugeneTestArea.py
+++ eugeneTestArea.py
@@ -1,16 +1,46 @@
-from bs4 import BeautifulSoup
-import urllib2
+"""Example of how to use wx tooltips on a matplotlib figure window.
+Adapted from http://osdir.com/ml/python.matplotlib.devel/2006-09/msg00048.html"""
 
-opener = urllib2.build_opener()
-resource = opener.open("http://apps.shareholder.com/sec/viewerContent.aspx?companyid=AAPL&docid=1199756")
-data = resource.read()
-resource.close()
+import matplotlib as plt
+plt.use('WXAgg')
+plt.interactive(False)
 
-soup = BeautifulSoup(data)
-preTables = soup.findAll('pre')
-financialTables = preTables[1:] # skip first table
+import pylab as pl
+from pylab import get_current_fig_manager as gcfm
+import wx
+import numpy as np
+import random
 
-firstTableStr = str(financialTables[0])
+class wxToolTipExample(object):
+    def __init__(self):
+        self.figure = pl.figure()
+        self.axis = self.figure.add_subplot(111)
 
-newLineSplit = firstTableStr.split('\n')
-print newLineSplit
+        # create a long tooltip with newline to get around wx bug (in v2.6.3.3)
+        # where newlines aren't recognized on subsequent self.tooltip.SetTip() calls
+        self.tooltip = wx.ToolTip(tip='tip with a long %s line and a newline\n' % (' '*100))
+        gcfm().canvas.SetToolTip(self.tooltip)
+        self.tooltip.Enable(False)
+        self.tooltip.SetDelay(0)
+        self.figure.canvas.mpl_connect('motion_notify_event', self._onMotion)
+
+        self.dataX = np.arange(0, 100)
+        self.dataY = [random.random()*100.0 for x in xrange(len(self.dataX))]
+        self.axis.plot(self.dataX, self.dataY, linestyle='-', marker='o', markersize=20, label='myplot')
+
+    def _onMotion(self, event):
+        collisionFound = False
+        if event.xdata != None and event.ydata != None: # mouse is inside the axes
+            for i in xrange(len(self.dataX)):
+                radius = 1
+                if abs(event.xdata - self.dataX[i]) < radius and abs(event.ydata - self.dataY[i]) < radius:
+                    top = tip='x=%f\ny=%f' % (event.xdata, event.ydata)
+                    self.tooltip.SetTip(tip) 
+                    self.tooltip.Enable(True)
+                    collisionFound = True
+                    break
+        if not collisionFound:
+            self.tooltip.Enable(False)
+
+example = wxToolTipExample()
+pl.show()
--- stock-slope.py
+++ stock-slope.py
@@ -23,7 +23,8 @@ class Date:
             self.y = int(splitDate[2])
 
     def __repr__(self):
-        return str('Date(' + self.date + ')')
+        # return str('Date(' + self.date + ')')
+        return str(self.date)
 
     # returns (start date, end date)
     def dateRange(self, daysPadding): # daysPadding is in days
@@ -335,8 +336,9 @@ class Query:
 
     def plotIndividualStockDifferences(self):
         import matplotlib.pyplot as plt
-        self.dataFrame.plot(use_index=True, y='Individual Stock Difference')
-        plt.show()
+        plot = mouseHoverPlot(self.dataFrame['Individual Stock Difference'], self.dataFrame)
+        pl.ylabel('Stock Difference over %d day(s) in dollars' % self.daysPadding)
+        pl.show()
 
     def plotSlopeChanges(self):
         import matplotlib.pyplot as plt
@@ -431,6 +433,51 @@ class Query:
         maxProductRow = self.dataFrame[self.dataFrame.index == maxProduct]
         return maxProductRow
 
+import matplotlib as plt
+plt.use('WXAgg')
+plt.interactive(False)
+
+import pylab as pl
+from pylab import get_current_fig_manager as gcfm
+import wx
+import numpy as np
+import random
+
+class mouseHoverPlot(object):
+    def __init__(self, dataY, dataFrame):
+        self.dataFrame = dataFrame
+        self.figure = pl.figure()
+        self.axis = self.figure.add_subplot(111)
+        # create a long tooltip with newline to get around wx bug (in v2.6.3.3)
+        # where newlines aren't recognized on subsequent self.tooltip.SetTip() calls
+        self.tooltip = wx.ToolTip(tip='tip with a long %s line and a newline\n' % (' '*100))
+        gcfm().canvas.SetToolTip(self.tooltip)
+        self.tooltip.Enable(False)
+        self.tooltip.SetDelay(0)
+        self.figure.canvas.mpl_connect('motion_notify_event', self._onMotion)
+        self.dataX = range(len(dataY))
+        self.dataY = dataY
+        self.xTicks = dataFrame.index
+        pl.xticks(self.dataX, self.xTicks)
+        self.axis.plot(self.dataX, self.dataY, linestyle='-', marker='o', markersize=15, label='myplot')
+
+    def _onMotion(self, event):
+        collisionFound = False
+        if event.xdata != None and event.ydata != None: # mouse is inside the axes
+            for i in xrange(len(self.dataX)):
+                radius = .2
+                if abs(event.xdata - self.dataX[i]) < radius and abs(event.ydata - self.dataY[i]) < radius:
+                    def productName(xPos):
+                        return self.xTicks[int(round(xPos))]
+                    # print self.dataFrame[self.dataFrame.index == productName(event.xdata)]['Release Date'][0]
+                    top = tip='Product: %s\nRelease Date: %s\nStock Price Difference: $%.2f' % (productName(event.xdata),self.dataFrame[self.dataFrame.index == productName(event.xdata)]['Release Date'][0], self.dataFrame[self.dataFrame.index == productName(event.xdata)]['Individual Stock Difference'][0] )
+                    self.tooltip.SetTip(tip) 
+                    self.tooltip.Enable(True)
+                    collisionFound = True
+                    break
+        if not collisionFound:
+            self.tooltip.Enable(False)
+
 datahash = getProductReleasesForApple()
 
 timeline = datahash
@@ -466,4 +513,6 @@ timelineDataFrame = removeOldItems(earliestDate)
 # sampleQuery.plotSlopeChanges()
 
 sampleFamilyQuery = Query("family", 'Drives')
-# sampleFamilyQuery.plotIndividualStockDifferences()
+sampleFamilyQuery.plotIndividualStockDifferences()
+
+# print timelineDataFrame['Macintosh plus (Platinum)']
